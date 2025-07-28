7974f4409b973fe93c96ce52a050e6b56fdd41b5
joji <jfujika.t@gmail.com>
2025-07-27 22:40:16 -0700

    Make input setting for AI collapseable and added Bootstrap script and Types

--- src/components/llm/llm.tsx
+++ src/components/llm/llm.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import "./llm.css";
 
 interface LLMProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface LLMBtnProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const fetchDataFromLLM = async (msg: object, llmURL: string = '') => {
   const urlEndPoint:string = llmURL || 'http://127.0.0.1:11434/api/chat';
@@ -46,11 +48,14 @@ const parseJSONString = (jsonString: string) => {
   }
 }
 
+let aiBtnTextState:boolean = false;
+
 const LLM: FC<LLMProps> = () => {
     const [userPrompt, setUserPrompt] = useState('');
     const [aiResponse, setAIResponse] = useState('');
     const [llmEndpoint, setllmEndpoint] = useState('');
     const [llmModel, setllmModel] = useState('gemma3:1b');
+    const [btnAISettingText, setBtnAISettingText] = useState('Show AI Setting');
 
     const invokeAILLMapi = async () => {
       const msg:object = {
@@ -59,6 +64,7 @@ const LLM: FC<LLMProps> = () => {
       };
       const response:string = await fetchDataFromLLM(msg, llmEndpoint);
       const formatResponse:any = JSON.parse(parseJSONString(response));
+      console.log(formatResponse);
       const len = formatResponse.length;
       let aiResponse:string = '';
       for (let i = 0; i < len; i++) {
@@ -78,13 +84,27 @@ const LLM: FC<LLMProps> = () => {
     const handleLLMModel = (ev: { target: { value: React.SetStateAction<string>; }; }) => {
       setllmModel(ev.target.value);
     }
+
+    const handleBtnAISetting = () => {
+      aiBtnTextState = !aiBtnTextState;
+      if (aiBtnTextState) {
+        setBtnAISettingText('Hide AI Setting');
+      } else {
+        setBtnAISettingText('Show AI Setting');
+      }
+    }
     
     return <div>
-        <div className='llm-header'>React LLM</div>
-        <div className='container'>
+        <div className="llm-header">React LLM</div>
+        <div className="container">
           <div className="form-floating">
             <div className="form-setting">
-              <div className="mb-3">
+              <div className="collapse-setting">
+                <button className="btn btn-primary" type="button" onClick={handleBtnAISetting} data-bs-toggle="collapse" data-bs-target="#aiSettingCollapse" aria-expanded="false" aria-controls="aiSettingCollapse">
+                  {btnAISettingText}
+                </button>
+                <div className="collapse" id="aiSettingCollapse">
+                  <div className="mb-3">
                 <label htmlFor="llmEndPointInput" className="form-label">LLM Endpoint</label>
                 <input className="form-control llm-endpoint-input" value={llmEndpoint} onChange={handleLLMEndpoint} id="llmEndPointInput" />
               </div>
@@ -92,7 +112,10 @@ const LLM: FC<LLMProps> = () => {
                 <label htmlFor="llmModelInput" className="form-label">LLM Model</label>
                 <input className="form-control llm-model-input" value={llmModel} onChange={handleLLMModel} id="llmModelInput" />
               </div>
+                </div>
+              </div>
             </div>
+            <div className="blank-divider"></div>
             <div className="form-floating">
               <textarea 
               id="floatingTextarea2"
@@ -103,8 +126,8 @@ const LLM: FC<LLMProps> = () => {
               <label htmlFor="floatingTextarea2">Ask AI</label>
             </div>
           </div>
-          <button type="button" onClick={invokeAILLMapi} className="invoke-btn btn btn-primary">Invoke</button>
-          <div className='AI_response container-lg'><p>{aiResponse}</p></div>
+          <button type="button" onClick={invokeAILLMapi} className="invoke-btn btn btn-primary">Ask</button>
+          <div className="AI_response container-lg"><p>{aiResponse}</p></div>
         </div>
     </div>;
 }
